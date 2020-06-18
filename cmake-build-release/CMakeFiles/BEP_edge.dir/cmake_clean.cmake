9b49792a3e85d8526823b0f91614e8a63f5cddea
ThijsLacquet <thijs.lacquet@gmail.com>
2020-06-19 00:51:24 +0200

    Added release

---
+++ cmake-build-release/CMakeFiles/BEP_edge.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "BEP_edge"
+  "BEP_edge.pdb"
+  "CMakeFiles/BEP_edge.dir/Functions/FFT.cpp.o"
+  "CMakeFiles/BEP_edge.dir/Functions/OFDM.cpp.o"
+  "CMakeFiles/BEP_edge.dir/Functions/QAM.cpp.o"
+  "CMakeFiles/BEP_edge.dir/Network/TCPClient.cpp.o"
+  "CMakeFiles/BEP_edge.dir/Network/TCPServer.cpp.o"
+  "CMakeFiles/BEP_edge.dir/Network/UDP.cpp.o"
+  "CMakeFiles/BEP_edge.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/BEP_edge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
